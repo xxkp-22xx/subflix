a48ecebe79766e21fe6bd0dca93187f1502552f8
aryan1769 <omg.aryan691@gmail.com>
2025-04-08 13:56:44 -0600

    Sepolia Added

---
+++ my-app/src/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    LOCAL: {
+      address: "0xYourGanacheContractAddressHere",
+      networkId: "1337", // Ganache network ID
+    },
+    SEPOLIA: {
+      address: "0x4d40fE141649Cc8cC1a46631b8871429a9CA3Cb7",
+      networkId: "11155111", // Sepolia network ID
+    }
+  };
+  
+  export default CONFIG;
+  
--- my-app/src/utils/contract.jsx
+++ my-app/src/utils/contract.jsx
@@ -1,17 +1,23 @@
 import Web3 from 'web3';
 import ContractABI from '../abi/SubManager.json';
-
-const CONTRACT_ADDRESS = "0x8f451cb17cD302Ce56a7D3cCcC6cD4f570eB0AA4";
+import CONFIG from '../config';
 
 const getContract = async () => {
-  if (window.ethereum) {
-    const web3 = new Web3(window.ethereum);
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    return new web3.eth.Contract(ContractABI.abi, CONTRACT_ADDRESS);
-  } else {
+  if (!window.ethereum) {
     alert("Please install MetaMask");
     throw new Error("Ethereum provider not found");
   }
+
+  const web3 = new Web3(window.ethereum);
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const networkId = await web3.eth.net.getId();
+
+  const network =
+    networkId === parseInt(CONFIG.SEPOLIA.networkId)
+      ? CONFIG.SEPOLIA
+      : CONFIG.LOCAL;
+
+  return new web3.eth.Contract(ContractABI.abi, network.address);
 };
 
 export default getContract;
